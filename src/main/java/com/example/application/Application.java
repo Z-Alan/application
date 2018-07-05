588264cb701c82dbb6c13afec941185612745599
zhouqiang <nqws1002@163.com>
2018-07-05 10:06:18 +0800

    初始化项目

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,17 @@
+package com.example.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Application.
+ * <p>启动类</p>
+ * @author       zhouqiang
+ * @date   2018/7/5 9:55
+ * */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
