76f77c8d37e7df31f7b8225387cea630c723d5d6
zhouqiang <nqws1002@163.com>
2018-08-02 18:17:47 +0800

    修改方法

--- src/main/java/com/example/util/DateUtil.java
+++ src/main/java/com/example/util/DateUtil.java
@@ -16,40 +16,32 @@ import java.time.format.DateTimeFormatter;
 public class DateUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(DateUtil.class);
 
+
+    private static final String DATE_TIME = "yyyy-MM-dd HH:mm:ss";
+
+
     /**
      * formatInstantToLocalDateTime.
+     *
      * @param instant 时间戳字符串 1354838400
      * @return 转换后的标准localDateTime eg. 2018-06-21T18:01:04
      * @author zhouqiang
-     * */
-    public static LocalDateTime formatInstantToLocalDateTime(String instant,String ofPattern){
-        LOGGER.info("[- 参数 -] --- [- {} -]",instant);
+     */
+    public static LocalDateTime formatInstantToLocalDateTime(String instant, String ofPattern) {
+        LOGGER.info("[- 参数 -] --- [- {} -]", instant);
         long timestamp = Long.parseLong(instant);
-        LOGGER.info("[- 临时变量 -] 时间戳String转换long类型 --- [- {} -]",timestamp);
-        DateTimeFormatter df= DateTimeFormatter.ofPattern(ofPattern);
+        LOGGER.info("[- 临时变量 -] 时间戳String转换long类型 --- [- {} -]", timestamp);
+        DateTimeFormatter df = DateTimeFormatter.ofPattern(ofPattern);
         String timeTarget = df.format(LocalDateTime
-                .ofInstant(Instant.ofEpochMilli(timestamp),ZoneId.of("Asia/Shanghai")));
-        LOGGER.info("[- 格式时间 -] 规范格式 --- [- {} -]",timeTarget);
-        return LocalDateTime.parse(timeTarget,df);
+                .ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.of("Asia/Shanghai")));
+        LOGGER.info("[- 格式时间 -] 规范格式 --- [- {} -]", timeTarget);
+        return LocalDateTime.parse(timeTarget, df);
     }
-
-    /**
-     * formatInstantToLocalDateTime.
-     * @param dateTimeStr  时间字符串 1354838400
-     * @return 转换后的标准localDateTime eg. 2018-06-21T18:01:04(str)->2018-06-21T18:01:04(LocalDateTime)
-     * @author zhouqiang
-     * */
-    public static LocalDateTime formatStringToLocalDateTime(String dateTimeStr){
-        return LocalDateTime.parse(dateTimeStr,DateTimeFormatter.ISO_DATE_TIME);
-    }
-
     public static void main(String[] args) {
 
-        LocalDateTime dateTime = DateUtil.formatInstantToLocalDateTime("1354838400","yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateTime = DateUtil.formatInstantToLocalDateTime("1354838400", DATE_TIME);
 
-        dateTime = DateUtil.formatStringToLocalDateTime("2018-06-21T18:01:04");
-        LOGGER.info("[- 结果 -] 时间戳转LocalDateTime --- [- {} -]",dateTime);
-
+        LOGGER.info("[- 结果 -] 时间戳转LocalDateTime --- [- {} -]", dateTime);
     }
 
 }
